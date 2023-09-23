9704e328765517fe17ddcf0aa3b8277663230261
csadmin <Vishnuteja.RachamaduguEXTERNAL@westpharma.com>
2023-09-23 18:16:46 +0530

    Initial commit

---
+++ app/src/main/java/com/ShehrozInClass/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.ShehrozInClass
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.ShehrozInClass.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var selectedColor: String
+    lateinit var selectedValueSpinner: String
+    lateinit var bundle: Bundle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setTitle(R.string.app_name)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.spinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.spinner_array)
+        )
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.rdRed.isChecked = true
+        binding.spinner.setSelection(0)
+        binding.rdRed.setOnClickListener {
+            onRadioButtonClicked(it)
+        }
+        binding.rdGreen.setOnClickListener {
+            onRadioButtonClicked(it)
+        }
+        binding.rdBlue.setOnClickListener {
+            onRadioButtonClicked(it)
+        }
+        binding.btViewtheresult.setOnClickListener {
+            bundle = Bundle()
+            bundle.putString("color", selectedColor)
+            bundle.putString("selectedValueSpinner", selectedValueSpinner)
+            val intent = Intent(this, VisualizeActivity::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
+        }
+
+        binding.spinner.selected {
+            selectedValueSpinner = resources.getStringArray(R.array.spinner_array)[it]
+        }
+    }
+    private fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            when (view.getId()) {
+                R.id.rd_red -> {
+                    selectedColor = getString(R.string.red)
+                }
+                R.id.rd_green -> {
+                    selectedColor = getString(R.string.green)
+                }
+                R.id.rd_blue -> {
+                    selectedColor = getString(R.string.blue)
+                }
+            }
+        }
+    }
+
+    fun Spinner.selected(action: (position: Int) -> Unit) {
+        this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                action(position)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ShehrozInClass/VisualizeActivity.kt
@@ -0,0 +1,55 @@
+package com.ShehrozInClass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.ShehrozInClass.databinding.ActivityVisualizeBinding
+
+class VisualizeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityVisualizeBinding
+    lateinit var selectedColor: String
+    lateinit var selectedSpinner: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visualize)
+        binding = ActivityVisualizeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val extras = this.intent.extras
+        selectedColor = extras?.getString("color").toString()
+        selectedSpinner = extras?.getString("selectedValueSpinner").toString()
+        binding.txtResult.text = selectedColor
+        setTextColor()
+        setBackgroundColor()
+        binding.btColose.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setBackgroundColor() {
+        when (selectedSpinner) {
+            "White" -> {
+              binding.root.setBackgroundColor(resources.getColor(R.color.white))
+            }
+            "Light Gray" -> {
+                binding.root.setBackgroundColor(resources.getColor(R.color.light_gray))
+            }
+
+        }
+    }
+
+    private fun setTextColor() {
+        when (selectedColor) {
+            "Red" -> {
+                binding.txtResult.setTextColor(resources.getColor(R.color.red))
+            }
+            "Green" -> {
+                binding.txtResult.setTextColor(resources.getColor(R.color.green))
+
+            }
+            "Blue" -> {
+                binding.txtResult.setTextColor(resources.getColor(R.color.blue))
+
+            }
+        }
+    }
+}
